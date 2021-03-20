6f16f684eee9091d85b24fa9105808507c4237e6
Nathany Mendonça <69328762+nathanymendonca@users.noreply.github.com>
2021-03-19 22:56:44 -0300

    inicializando servidor e aplicação

---
+++ src/main/java/com/validador/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.validador.cpf.validadorCpf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+
+    @GetMapping("/valida-cpf")
+    public String validaCpf(){
+        return "home/validaCpf";
+    }
+
+	}
+
